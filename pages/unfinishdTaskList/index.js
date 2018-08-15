5aedc450223b8a2a052922af1505b6a90e1c1950
D1N910 <wyd1n910@gmail.com>
2018-08-16 00:23:37 +0800

    进行中的任务已经罗列出来

--- pages/unfinishdTaskList/index.js
+++ pages/unfinishdTaskList/index.js
@@ -5,14 +5,49 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    unfinishedTaskLists:[
+
+    ]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    // 获取当前时间
+    var thisDate = new Date()
+    // 获取当前时间戳
+    var thisTime = thisDate.getTime()
+    // 获取所有任务
+    var allTasks = wx.getStorageSync('allTasks')
+
+    // 遍历所有任务
+    for (let i in wx.getStorageSync('allTasks')) {
+      // 如果结束时间大于等于当前时间，说明是在进行中的任务
+      if (thisTime <= allTasks[i].endAt) {
+        // 先得到任务开始/结束时间
+        var allTaskStart = new Date(allTasks[i].startAt)
+        var allTaskEnd = new Date(allTasks[i].endAt)
+
+        // 任务子项目
+        let unfinishedTaskListsItem = {}
+        // 赋值
+        unfinishedTaskListsItem.id = allTasks[i].id
+        unfinishedTaskListsItem.content = allTasks[i].content
+        // 转换时间
+        unfinishedTaskListsItem.startAt = `${allTaskStart.getFullYear()}年${allTaskStart.getMonth() + 1}月${allTaskStart.getDate()}日`
+        unfinishedTaskListsItem.endAt = `${allTaskEnd.getFullYear()}年${allTaskEnd.getMonth() + 1}月${allTaskEnd.getDate()}日`
+
+        // 完成时间
+        var doneDay = (allTasks[i].endAt - allTasks[i].startAt)/86400000 + 1
+        // 任务进度
+        unfinishedTaskListsItem.speedOfProgress =parseInt(allTasks[i].success.length / doneDay * 100)
+        this.data.unfinishedTaskLists.push(unfinishedTaskListsItem)
+      }
+    }
+    this.setData({
+      unfinishedTaskLists: this.data.unfinishedTaskLists
+    })
   },
 
   /**

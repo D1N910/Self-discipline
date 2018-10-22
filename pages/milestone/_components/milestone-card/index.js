0948cd0ecd5f5b438e7782f7ddc7f151a2670773
D1n910 <wyd1n910@gmail.com>
2018-10-22 20:14:06 +0800

    自律表做了启动页面

---
+++ pages/milestone/_components/milestone-card/index.js
@@ -0,0 +1,34 @@
+// pages/milestone/_components/milestone-card/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    title: {
+      type: String,
+      value: ''
+    },
+    startTime: {
+      type: String,
+      value: '' 
+    },
+    endTime: {
+      type: String,
+      value: ''
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
